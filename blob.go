0ff528a546a38436c74a06fb814cdf819f8ae1c4
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-05-08 14:44:54 +0200

    Add basic handlers for ints and byte streams as noms Values

---
+++ blob.go
@@ -0,0 +1,21 @@
+package backing
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/oneiro-ndev/noms/go/datas"
+	nt "github.com/oneiro-ndev/noms/go/types"
+	"github.com/pkg/errors"
+)
+
+// Blob creates a noms blob from a byte slice
+func Blob(db datas.Database, bs []byte) nt.Blob {
+	return nt.NewBlob(db, bytes.NewReader(bs))
+}
+
+// Unblob creates a byte slice from a noms blob
+func Unblob(blob nt.Blob) ([]byte, error) {
+	bytes, err := ioutil.ReadAll(blob.Reader())
+	return bytes, errors.Wrap(err, "types.Unblob failed")
+}
---
+++ int.go
@@ -0,0 +1,34 @@
+package backing
+
+import (
+	"encoding/binary"
+
+	"github.com/oneiro-ndev/noms/go/datas"
+	nt "github.com/oneiro-ndev/noms/go/types"
+)
+
+// Int is an integer which can serialize itself to a noms blob.
+//
+// Noms doesn't provide a native integer type, so it's on us
+// to handle this case. We manage it by providing this type wrapper which
+// knows how to serialize itself to and from `[]byte`.
+//
+// Simple big-endian serialization is used.
+type Int int64
+
+// ToBlob converts this Int into a Blob datatype
+func (b Int) ToBlob(db datas.Database) nt.Blob {
+	bytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytes, uint64(b))
+	return Blob(db, bytes)
+}
+
+// IntFromBlob creates a Int from a blob, if possible
+func IntFromBlob(blob nt.Blob) (Int, error) {
+	bytes, err := Unblob(blob)
+	if err != nil {
+		return Int(0), err
+	}
+
+	return Int(int64(binary.BigEndian.Uint64(bytes))), nil
+}

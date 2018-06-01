dd27bb93f005c5e22270e9da05d2453b85104abe
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-06-01 14:08:41 +0200

    Implement noms' Marshaler and Unmarshaler interfaces for Int
    This paves the way toward using the Marshal and Unmarshal methods
    to store complex data structures in noms.

--- blob.go
+++ blob.go
@@ -4,18 +4,19 @@ import (
 	"bytes"
 	"io/ioutil"
 
-	"github.com/attic-labs/noms/go/datas"
 	nt "github.com/attic-labs/noms/go/types"
 	"github.com/pkg/errors"
 )
 
 // Blob creates a noms blob from a byte slice
-func Blob(db datas.Database, bs []byte) nt.Blob {
-	return nt.NewBlob(db, bytes.NewReader(bs))
+//
+// Note: any datas.Database satisfies nt.ValueReadWriter
+func Blob(vrw nt.ValueReadWriter, bs []byte) nt.Blob {
+	return nt.NewBlob(vrw, bytes.NewReader(bs))
 }
 
 // Unblob creates a byte slice from a noms blob
 func Unblob(blob nt.Blob) ([]byte, error) {
 	bytes, err := ioutil.ReadAll(blob.Reader())
-	return bytes, errors.Wrap(err, "types.Unblob failed")
+	return bytes, errors.Wrap(err, "Unblob failed")
 }

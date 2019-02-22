0d7077175ed2e75b23db9ff014a87d96a1ee9162
Kent Quirk <kentquirk@gmail.com>
2019-02-21 22:16:19 -0500

    Noms doesn't like []byte; use strings instead

--- int.go
+++ int.go
@@ -2,6 +2,7 @@ package backing
 
 import (
 	"encoding/binary"
+	"strconv"
 
 	"github.com/attic-labs/noms/go/marshal"
 	nt "github.com/attic-labs/noms/go/types"
@@ -11,12 +12,14 @@ import (
 //
 // Noms doesn't provide a native integer type, so it's on us
 // to handle this case. We manage it by providing this type wrapper which
-// knows how to serialize itself to and from `[]byte`.
+// knows how to serialize itself to and from a base-36 string, which
+// turns out to be enormously faster in noms than an array of bytes.
 //
 // Simple big-endian serialization is used.
 type Int int64
 
-// ToBlob converts this Int into a Blob datatype
+// ToBlob converts this Int into a Blob datatype, which is used
+// in encoding/decoding the validator set.
 //
 // any datas.Database satisfies nt.ValueReadWriter
 func (i Int) ToBlob(vrw nt.ValueReadWriter) nt.Blob {
@@ -37,7 +40,8 @@ func IntFromBlob(blob nt.Blob) (Int, error) {
 
 // MarshalNoms satisfies the marshal.Marshaler interface
 func (i Int) MarshalNoms(vrw nt.ValueReadWriter) (val nt.Value, err error) {
-	return i.ToBlob(vrw), nil
+	n := strconv.FormatInt(int64(i), 36)
+	return marshal.Marshal(vrw, n)
 }
 
 // static assert that Int satisfies marshal.Marshaler
@@ -45,8 +49,13 @@ var _ marshal.Marshaler = (*Int)(nil)
 
 // UnmarshalNoms satisfies the marshal.Unmarshaler interface
 func (i *Int) UnmarshalNoms(v nt.Value) (err error) {
-	*i, err = IntFromBlob(v.(nt.Blob))
-	return
+	ns := v.(nt.String)
+	i64, err := strconv.ParseInt(string(ns), 36, 64)
+	if err != nil {
+		return err
+	}
+	*i = Int(i64)
+	return nil
 }
 
 // static assert that Int satisfies marshal.Marshaler

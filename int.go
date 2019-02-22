6c00b7444414ea55e6ee1ee434d6be0cd30b7390
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-02-22 10:54:57 +0100

    further simplify / speedup Ints

--- int.go
+++ int.go
@@ -1,55 +1,57 @@
 package backing
 
 import (
-	"encoding/binary"
+	"fmt"
+	"reflect"
 	"strconv"
 
 	"github.com/attic-labs/noms/go/marshal"
 	nt "github.com/attic-labs/noms/go/types"
 )
 
-// Int is an integer which can serialize itself to a noms blob.
+// Int is an integer which can serialize itself to noms.
 //
 // Noms doesn't provide a native integer type, so it's on us
 // to handle this case. We manage it by providing this type wrapper which
 // knows how to serialize itself to and from a base-36 string, which
 // turns out to be enormously faster in noms than an array of bytes.
-//
-// Simple big-endian serialization is used.
 type Int int64
 
-// ToBlob converts this Int into a Blob datatype, which is used
-// in encoding/decoding the validator set.
+// NomsValue creates a nt.Value from this Int
 //
-// any datas.Database satisfies nt.ValueReadWriter
-func (i Int) ToBlob(vrw nt.ValueReadWriter) nt.Blob {
-	bytes := make([]byte, 8)
-	binary.BigEndian.PutUint64(bytes, uint64(i))
-	return Blob(vrw, bytes)
-}
-
-// IntFromBlob creates a Int from a blob, if possible
-func IntFromBlob(blob nt.Blob) (Int, error) {
-	bytes, err := Unblob(blob)
-	if err != nil {
-		return Int(0), err
-	}
-
-	return Int(int64(binary.BigEndian.Uint64(bytes))), nil
+// As this operation cannot fail, we can expose a simpler interface than
+// MarshalNoms.
+func (i Int) NomsValue() nt.Value {
+	return nt.String(strconv.FormatInt(int64(i), 36))
 }
 
 // MarshalNoms satisfies the marshal.Marshaler interface
 func (i Int) MarshalNoms(vrw nt.ValueReadWriter) (val nt.Value, err error) {
-	n := strconv.FormatInt(int64(i), 36)
-	return marshal.Marshal(vrw, n)
+	return i.NomsValue(), nil
 }
 
 // static assert that Int satisfies marshal.Marshaler
 var _ marshal.Marshaler = (*Int)(nil)
 
+// IntFrom is a bit of sugar to simplify creating an Int from a nt.Value
+func IntFrom(v nt.Value) (Int, error) {
+	n := Int(0)
+	err := n.UnmarshalNoms(v)
+	return n, err
+}
+
 // UnmarshalNoms satisfies the marshal.Unmarshaler interface
 func (i *Int) UnmarshalNoms(v nt.Value) (err error) {
-	ns := v.(nt.String)
+	if i == nil {
+		return fmt.Errorf("Int.UnmarshalNoms called on nil int")
+	}
+	ns, ok := v.(nt.String)
+	if !ok {
+		return fmt.Errorf(
+			"Int.UnmarshalNoms value parameter must be nt.String; was %s",
+			reflect.ValueOf(v).Type(),
+		)
+	}
 	i64, err := strconv.ParseInt(string(ns), 36, 64)
 	if err != nil {
 		return err
